c9e044c8b1218b43d0712542f18b56ab45e8205a
Denandra Prasetya <denandra1628@gmail.com>
2016-08-18 22:54:10 +0800

    Contul

--- app/src/main/java/com/zonamagang/zonamagang/DetailSiswaOlehIndustri.java
+++ app/src/main/java/com/zonamagang/zonamagang/DetailSiswaOlehIndustri.java
@@ -2,6 +2,7 @@ package com.zonamagang.zonamagang;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class DetailSiswaOlehIndustri extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class DetailSiswaOlehIndustri extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_siswa_oleh_industri);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
