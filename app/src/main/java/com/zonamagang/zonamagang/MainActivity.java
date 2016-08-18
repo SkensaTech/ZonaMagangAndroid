391501dad68c39c6b01fe7dcff06030cb6e53883
AdityaHerlambang <herlambang.aditya456@gmail.com>
2016-08-18 21:17:50 +0800

    Update constants, add backendless SDK & init

--- app/src/main/java/com/zonamagang/zonamagang/MainActivity.java
+++ app/src/main/java/com/zonamagang/zonamagang/MainActivity.java
@@ -3,11 +3,14 @@ package com.zonamagang.zonamagang;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.backendless.Backendless;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Backendless.initApp(this,Constants.APP_ID,Constants.APP_SECRET,Constants.APP_VERSION);
     }
 }
