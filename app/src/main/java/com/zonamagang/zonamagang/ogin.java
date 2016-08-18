de97894dacf81fc253d42d6c2954b674cb6b7616
riskysaprilianti <riskysapriliantiputri2000@gmail.com>
2016-08-18 11:06:58 +0800

    login layout

---
+++ app/src/main/java/com/zonamagang/zonamagang/ogin.java
@@ -0,0 +1,13 @@
+package com.zonamagang.zonamagang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ogin);
+    }
+}
