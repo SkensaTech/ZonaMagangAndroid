e7b192c7dee04a6f192c1f82bfddbebcf05508a8
riskysaprilianti <riskysapriliantiputri2000@gmail.com>
2016-08-18 11:11:21 +0800

    login layout

--- app/src/main/java/com/zonamagang/zonamagang/login.java
+++ app/src/main/java/com/zonamagang/zonamagang/login.java
@@ -3,11 +3,11 @@ package com.zonamagang.zonamagang;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ogin extends AppCompatActivity {
+public class login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ogin);
+        setContentView(R.layout.activity_login);
     }
 }
