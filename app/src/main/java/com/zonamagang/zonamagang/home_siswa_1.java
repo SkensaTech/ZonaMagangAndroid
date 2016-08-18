27515414c34e6bc89f25b55490895bc9062ba0c8
suparsana21 <supoa>
2016-08-18 15:53:32 +0800

    Belajar

--- app/src/main/java/com/zonamagang/zonamagang/home_siswa_1.java
+++ app/src/main/java/com/zonamagang/zonamagang/home_siswa_1.java
@@ -4,19 +4,20 @@ package com.zonamagang.zonamagang;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class home_siswa_1 extends Activity {
+public class home_siswa_1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_siswa_1);
+
+        Toolbar x = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(x);
+
 
     }
 }
