e8eb6a49763e1b207e8ce657c4e95d56df12bdce
ASUS <suparsana2146@gmail.com>
2016-08-19 06:39:42 +0800

    Login - Register Menu

--- app/src/main/java/com/zonamagang/zonamagang/MainActivity.java
+++ app/src/main/java/com/zonamagang/zonamagang/MainActivity.java
@@ -1,16 +1,32 @@
 package com.zonamagang.zonamagang;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import com.backendless.Backendless;
+//import com.backenSdless.Backendless;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView x;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Backendless.initApp(this,Constants.APP_ID,Constants.APP_SECRET,Constants.APP_VERSION);
+        x = (TextView) findViewById(R.id.buatakun);
+
+        x.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent register = new Intent(getApplicationContext(),register1.class);
+                startActivity(register);
+            }
+        });
+       // Backendless.initApp(this,Constants.APP_ID,Constants.APP_SECRET,Constants.APP_VERSION);
+
+
     }
 }
