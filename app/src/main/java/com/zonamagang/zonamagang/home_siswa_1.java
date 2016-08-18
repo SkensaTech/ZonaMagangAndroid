3cd9e59e4935aaf11007e8bfc4b1b7bcb174391e
suparsana21 <supoa>
2016-08-18 10:02:07 +0800

    Belajar

--- app/src/main/java/com/zonamagang/zonamagang/home_siswa_1.java
+++ app/src/main/java/com/zonamagang/zonamagang/home_siswa_1.java
@@ -1,7 +1,9 @@
 package com.zonamagang.zonamagang;
 
 import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class home_siswa_1 extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class home_siswa_1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_siswa_1);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
