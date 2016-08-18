0385904304e94893404a9f5a6792523f0b9e684f
suparsana21 <supoa>
2016-08-18 09:42:42 +0800

    Belajar

--- app/src/main/java/com/zonamagang/zonamagang/home_siswa_1.java
+++ app/src/main/java/com/zonamagang/zonamagang/home_siswa_1.java
@@ -3,11 +3,11 @@ package com.zonamagang.zonamagang;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class home_siswa_1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home_siswa_1);
     }
 }
