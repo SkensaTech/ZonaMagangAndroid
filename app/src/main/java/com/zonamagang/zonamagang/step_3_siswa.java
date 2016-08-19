b3a7f199a2952ba7d92553a97c932188bc4cc2a7
wahyubaskara <wahyucrazy@gmail.com>
2016-08-19 17:20:43 +0800

    many bug fixes

--- app/src/main/java/com/zonamagang/zonamagang/step_3_siswa.java
+++ app/src/main/java/com/zonamagang/zonamagang/step_3_siswa.java
@@ -1,21 +1,72 @@
 package com.zonamagang.zonamagang;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class step_3_siswa extends AppCompatActivity {
+
+    ImageView mLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step_3_siswa);
-
+        this.layoutItems();
         //coding untuk toolbar
         Toolbar x = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(x);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
+    //coding upload foto
+    public void showFileChooser(View view) {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, 1);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1:
+            {
+                if (resultCode == RESULT_OK) {
+                    Uri photoUri = data.getData();
+                    if (photoUri != null) {
+                        try {
+                            String[] filePathColumn = {
+                                    MediaStore.Images.Media.DATA
+                            };
+                            Cursor cursor = getContentResolver().query(photoUri, filePathColumn, null, null, null);
+                            cursor.moveToFirst();
+                            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                            String filePath = cursor.getString(columnIndex);
+                            cursor.close();
+                            Bitmap bMap_image = BitmapFactory.decodeFile(filePath);
+                            mLogo.setImageBitmap(bMap_image);
+
+
+                        } catch (Exception e) {}
+                    }
+                }
+            }
+        }
+    }
+    //end coding upload foto
+
+    private void layoutItems() {
+        mLogo = (ImageView) findViewById(R.id.step_3_siswa_logo);
 
     }
 }
