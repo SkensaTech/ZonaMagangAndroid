303313feba338b8cd138dc6d47f59303d9b63373
Denandra Prasetya <denandra1628@gmail.com>
2016-08-19 18:01:06 +0800

    Drawer setengah jadi (denandra)

--- app/src/main/java/com/zonamagang/zonamagang/HomeIndustri.java
+++ app/src/main/java/com/zonamagang/zonamagang/HomeIndustri.java
@@ -3,15 +3,30 @@ package com.zonamagang.zonamagang;
 import android.support.design.widget.TabLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 public class HomeIndustri extends AppCompatActivity {
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
     private ViewPageAdapter adapter;
+
+    private Drawer result = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,18 +42,6 @@ public class HomeIndustri extends AppCompatActivity {
         adapter = new ViewPageAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
 
-//        final TabLayout.Tab Semua = tabLayout.newTab();
-//        final TabLayout.Tab Belum = tabLayout.newTab();
-//        final TabLayout.Tab Sudah = tabLayout.newTab();
-//
-//        Semua.setText("Semua Data");
-//        Belum.setText("Belum");
-//        Sudah.setText("Sudah");
-//
-//        tabLayout.addTab(Semua, 0);
-//        tabLayout.addTab(Belum, 1);
-//        tabLayout.addTab(Sudah, 2);
-
         TabLayout tabLayout = (TabLayout)findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
 
@@ -48,5 +51,28 @@ public class HomeIndustri extends AppCompatActivity {
         tabLayout.setSelectedTabIndicatorColor(ContextCompat.getColor(this, R.color.colorPrimaryLight));
 
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName("Test 1");
+        SecondaryDrawerItem item2 = (SecondaryDrawerItem) new SecondaryDrawerItem().withIdentifier(2).withName("Test 2");
+
+        result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(myToolbar)
+                .addDrawerItems(
+                        item1,
+                        new DividerDrawerItem(),
+                        item2,
+                        new DividerDrawerItem()
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        return false;
+                    }
+                })
+                .build();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        result.getActionBarDrawerToggle().setDrawerIndicatorEnabled(true);
     }
 }
