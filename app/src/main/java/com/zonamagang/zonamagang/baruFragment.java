33be5e22e445b624ca9cd3e1342a94e1f6be5f52
Denandra Prasetya <denandra1628@gmail.com>
2016-08-19 18:57:10 +0800

    Drawer setengah jadi (denandra)

--- app/src/main/java/com/zonamagang/zonamagang/baruFragment.java
+++ app/src/main/java/com/zonamagang/zonamagang/baruFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 
 /**
@@ -14,14 +15,14 @@ import android.widget.ListView;
 public class baruFragment extends Fragment{
 
     ListView listView;
+    private Button detail;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.baru, container, false);
-    }
 
-    public void DetailSiswa(){
+        Button detail = (Button)getView().findViewById(R.id.detailSiswa);
 
     }
 
