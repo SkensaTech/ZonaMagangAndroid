bf3d72daf16611416ff340361177b459531a7c28
AdityaHerlambang <herlambang.aditya456@gmail.com>
2016-08-19 08:18:08 +0800

    Add splash screen

--- app/src/main/java/com/zonamagang/zonamagang/SplashActivity.java
+++ app/src/main/java/com/zonamagang/zonamagang/SplashActivity.java
@@ -3,7 +3,6 @@ package com.zonamagang.zonamagang;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends Activity {
