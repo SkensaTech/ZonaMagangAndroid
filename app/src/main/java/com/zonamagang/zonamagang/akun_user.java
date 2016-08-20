a3e09e44bb7e8cdff8731749dc36381237a2ba8b
suparsana21 <supoa>
2016-08-20 12:47:50 +0800

    Complete Akun Saya

---
+++ app/src/main/java/com/zonamagang/zonamagang/akun_user.java
@@ -0,0 +1,23 @@
+package com.zonamagang.zonamagang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class akun_user extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_akun_user);
+        Toolbar xx = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(xx);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.settingmenu, menu);
+        return true;
+    }
+}
