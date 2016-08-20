d5603eef0780d085efb4b28e94531253819cae2c
Denandra Prasetya <denandra1628@gmail.com>
2016-08-20 08:12:53 +0800

    Drawer setengah jadi (denandra)

--- app/src/main/java/com/zonamagang/zonamagang/DetailSiswaOlehIndustri.java
+++ app/src/main/java/com/zonamagang/zonamagang/DetailSiswaOlehIndustri.java
@@ -1,8 +1,11 @@
 package com.zonamagang.zonamagang;
 
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class DetailSiswaOlehIndustri extends AppCompatActivity {
 
@@ -15,4 +18,15 @@ public class DetailSiswaOlehIndustri extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                Intent intent = new Intent(getApplicationContext(),HomeIndustri.class);
+                finish();
+                startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
