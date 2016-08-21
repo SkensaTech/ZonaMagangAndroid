8aae3f648bc1379e3954efcffd77c8fc1bee9ab7
suparsana21 <supoa>
2016-08-21 14:36:23 +0800

    Akun User

--- app/src/main/java/com/zonamagang/zonamagang/Notifikasi_Industri.java
+++ app/src/main/java/com/zonamagang/zonamagang/Notifikasi_Industri.java
@@ -2,6 +2,8 @@ package com.zonamagang.zonamagang;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 public class Notifikasi_Industri extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class Notifikasi_Industri extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifikasi__industri);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
