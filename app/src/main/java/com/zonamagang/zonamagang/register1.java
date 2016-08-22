af98e8245a19466643eee21d780b8375641bfb5d
suparsana21 <supoa>
2016-08-22 11:04:25 +0800

    Akun User

--- app/src/main/java/com/zonamagang/zonamagang/register1.java
+++ app/src/main/java/com/zonamagang/zonamagang/register1.java
@@ -3,34 +3,44 @@ package com.zonamagang.zonamagang;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
 public class register1 extends AppCompatActivity {
-    Button siswa,industri;
+
+    Button siswa, industri;
     String jenis_daftar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register1);
+
         siswa = (Button) findViewById(R.id.siswa);
-        industri= (Button) findViewById(R.id.industri);
+        industri = (Button) findViewById(R.id.industri);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         siswa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View x) {
-                Intent daftarsiswa = new Intent(getApplicationContext(),register2.class);
+                Intent daftarsiswa = new Intent(getApplicationContext(), register2.class);
                 jenis_daftar = "siswa";
-                daftarsiswa.putExtra("jenis_daftar",jenis_daftar);
+                daftarsiswa.putExtra("jenis_daftar", jenis_daftar);
                 startActivity(daftarsiswa);
             }
         });
         industri.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View x) {
-                Intent daftarindustri = new Intent(getApplicationContext(),register2.class);
+                Intent daftarindustri = new Intent(getApplicationContext(), register2.class);
                 jenis_daftar = "industri";
-                daftarindustri.putExtra("jenis_daftar",jenis_daftar);
+                daftarindustri.putExtra("jenis_daftar", jenis_daftar);
                 startActivity(daftarindustri);
             }
         });
@@ -38,11 +48,30 @@ public class register1 extends AppCompatActivity {
         masuk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Intent x = new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(x);
                 finish();
+                Intent x = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(x);
+
             }
         });
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                Intent intent = new Intent(register1.this,MainActivity.class);
+                startActivity(intent);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onBackPressed(){
+        finish();
+        Intent intent = new Intent(register1.this,MainActivity.class);
+        startActivity(intent);
     }
 }
